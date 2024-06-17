d3b4c4765e14b1ffb7043659a723521cc3c61f51
Salimov-dev <ruspb1987@gmail.com>
2024-06-17 15:11:48 +0300

    профиль пользователя настроен

--- src/app/components/layout/index.tsx
+++ src/app/components/layout/index.tsx
@@ -1,9 +1,26 @@
-import { Outlet } from "react-router-dom";
-import Container from "../container";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { Profile } from "../profile/profile";
+import { useSelector } from "react-redux";
+import {
+  selectUser,
+  selectIsAuthenticated
+} from "../../features/user/userSlice";
 import Header from "../header";
 import Navbar from "../navbar";
+import Container from "../container";
 
-const Layout = () => {
+export const Layout = () => {
+  const isAuthenticated = useSelector(selectIsAuthenticated);
+  const user = useSelector(selectUser);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate("/auth");
+    }
+  }, []);
+
   return (
     <>
       <Header />
@@ -14,9 +31,10 @@ const Layout = () => {
         <div className="flex-1 p-4">
           <Outlet />
         </div>
+        <div className="flex-2 p-4">
+          <div className="flex-col flex gap-5">{!user && <Profile />}</div>
+        </div>
       </Container>
     </>
   );
 };
-
-export default Layout;
