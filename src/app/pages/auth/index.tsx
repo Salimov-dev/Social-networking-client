b3c96db49b832cbaa239ba16ee6a1d19ba8b7d4f
Salimov-dev <ruspb1987@gmail.com>
2024-06-17 13:26:38 +0300

    авторизация настроена

--- src/app/pages/auth/index.tsx
+++ src/app/pages/auth/index.tsx
@@ -1,5 +1,33 @@
+import { Card, CardBody, Tab, Tabs } from "@nextui-org/react";
+import { useState } from "react";
+import { Login } from "../../features/user/login";
+import { Register } from "../../features/user/register";
+
 const Auth = () => {
-  return <h3>Auth</h3>;
+  const [selected, setSelected] = useState("login");
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="flex flex-col">
+        <Card className="max-w-full w-[340px] h-[450px]">
+          <CardBody className="overflow-hidden">
+            <Tabs
+              fullWidth
+              size="md"
+              selectedKey={selected}
+              onSelectionChange={(key) => setSelected(key as string)}
+            >
+              <Tab key="login" title="Вход">
+                <Login setSelected={setSelected} />
+              </Tab>
+              <Tab key="sing-up" title="Регистрация">
+                <Register setSelected={setSelected} />
+              </Tab>
+            </Tabs>
+          </CardBody>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
